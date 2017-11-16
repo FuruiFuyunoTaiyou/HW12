136e37fdc68c0d175185eac40a2727e2df920d31
Winnie Chen <wchen10@stuy.edu>
2017-11-16 11:01:06 -0500

    classwork

---
+++ hw12.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <time.h>
+
+
+
+int main(){
+  printf("initial message \n");
+  int f0 = fork();
+  int f1;
+  if(f0){
+    f1 = fork();
+    /*if(f1){
+      printf("pid: %d \n", getpid());
+      }*/
+  }
+  if(f0 && f1){
+    int status;
+    int finished = wait(&status);
+    printf("finished child: %d, sleeping for %d s \n", finished, 123456);
+  }
+  if(!f1 || !f0){
+    srand(time(NULL) + f0);
+    printf("child pid: %d \n", getpid());
+    int i = rand() % 16 + 5;
+    sleep(i);
+    printf("child %d finished \n", getpid());
+    return i;
+  }
+  return 0;
+}
